28cacee309bbd617fa1c046c25649a3fb302f7df
c4leixub <tonylei1223@gmail.com>
2017-07-20 21:36:36 -0700

    Update hackathon_copy.js

--- hackathon_copy.js
+++ hackathon_copy.js
@@ -342,7 +342,7 @@ var hackathon = {
 	bleachCares: ['Do Not Bleach'],
 
 	getCategory: function() {
-		console.log(document.getElementById('breadcrumb-back-link'));
+		window.console.log("dsfasdfasdf" + document.getElementById('breadcrumb-back-link'));
 		return '';
 	},
 	
